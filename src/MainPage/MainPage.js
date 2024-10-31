25154874004872cf6d6d7f1aa373ef39f2abbddd
ROlearyPro <rolearyprogramming@gmail.com>
2024-10-31 16:31:09 -0600

    Minor changes to routing.

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -3,13 +3,17 @@ import { useState, useEffect } from "react";
 import { Routes, Route, Link, useNavigate, useParams } from "react-router-dom";
 
 function MainPage() {
+  const navigate = useNavigate();
+  const params = useParams();
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        
           We'll be putting the main page here. Further routing will act similarly. 
-        </p>
-      
+          <Link to={`/Main/${params}/Stats`}>
+          For example, click here to navigate to the stats page.
+          </Link>
+          <Link to={`/Main/${params}/Train`}>Or click here to navigate to the training page.</Link>
       </header>
     </div>
   );
--- src/StatPage/StatPage.js
+++ src/StatPage/StatPage.js
@@ -9,14 +9,7 @@ function StatPage() {
         <p>
           StatPage
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
