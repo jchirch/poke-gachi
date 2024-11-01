31e5ddaf5f2532b0070f52d27d53db8aaa788405
ROlearyPro <rolearyprogramming@gmail.com>
2024-11-01 14:28:40 -0600

    Basic styling, implemented random backgrounds for main page (play area)

--- src/App/App.js
+++ src/App/App.js
@@ -7,13 +7,26 @@ import MainPage from '../MainPage/MainPage';
 import StartPage from '../StartPage/StartPage';
 import TrainPage from '../TrainPage/TrainPage';
 import StatPage from '../StatPage/StatPage';
-
+import beachImg from '../Utils/Images/Box_Beach_BDSP.png'
+import caveImg from '../Utils/Images/Box_Cave_BDSP.png'
+import checkImg from '../Utils/Images/Box_Checks_BDSP.png'
+import cityImg from '../Utils/Images/Box_City_BDSP.png'
+import cragImg from '../Utils/Images/Box_Crag_BDSP.png'
+import desertImg from '../Utils/Images/Box_Desert_BDSP.png'
+import forestImg from '../Utils/Images/Box_Forest_BDSP.png'
+import savannahImg from '../Utils/Images/Box_Savanna_BDSP.png'
+import seafloorImg from '../Utils/Images/Box_Seafloor_BDSP.png'
+import skyImg from '../Utils/Images/Box_Sky_BDSP.png'
+import snowImg from '../Utils/Images/Box_Snow_BDSP.png'
+import volcanoImg from '../Utils/Images/Box_Volcano_BDSP.png'
 function App() {
 
   const [user, setUser] = useState({});
   const [mons, setMons] = useState([]);
   const navigate = useNavigate();
-  let params = "this";  
+  let params = "this";
+  let bgArray = [beachImg, caveImg, checkImg, cityImg, cragImg, desertImg, forestImg, savannahImg, seafloorImg, skyImg, snowImg, volcanoImg]
+
   function fetchData() {
     // fetch()
     //   .then((res) => {
@@ -39,25 +52,42 @@ function App() {
     fetchData();
 
   }, [])
-
-
+  const playAreaPlaceholder = document.getElementById('.play-area');
+  let bgTemp = cityImg;
+  bgTemp = bgArray[Math.round(Math.random() * bgArray.length)];
+  if(playAreaPlaceholder){
+  playAreaPlaceholder.style.backgroundImage=bgTemp;}
   return (
     <div className="App">
-      <header className="App-header"> 
-          <Link to={'/'}>
+      <header className="App-header">
+        <Link to={'/'}>
           Return to start.</Link>
       </header>
-      <p onClick={()=>navigate(`/Start`)}>
-      Click here to navigate to the next part of the site.
-      </p>
+
+      <div className = 'play-container'>
+
+        Look below.
+      <div onClick={() => navigate(`/Start`)}>
+        Click here to navigate to the next part of the site.
+      </div>
+      </div>
+
+      <script type='text/javascript'>
+      console.log("should have run");
+
+          
+      </script>
       <Routes>
-      <Route path="/*" params={"this"} element={<ErrorPage params={params}/>} />
+        <Route path="/*" />
 
         <Route path="/Start" element={<StartPage />} />
         <Route path="/Main/:UserVal" element={<MainPage params={params} />} />
         <Route path="/Main/:UserVal/Stats" element={<StatPage params={params} />} />
         <Route path="/Main/:UserVal/Train" element={<TrainPage params={params} />} />
       </Routes>
+
+
+
     </div>
   );
 }
