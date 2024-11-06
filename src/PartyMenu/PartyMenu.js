19b49db20afd6e7cff0f89ecc10f3aa560f5fad6
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-11-05 22:29:25 -0700

    Feat: alternative party renders all in database

--- src/PartyMenu/PartyMenu.js
+++ src/PartyMenu/PartyMenu.js
@@ -24,25 +24,15 @@ function PartyMenu() {
     fetchData();
   }, [])
 
-  // if (pokemon1) {
-  //   console.log(pokemon1);
-  // }
   return (
     <div className="party-menu">
       <header className="party-header">
-        Your Pokémon!
+        Your Party!
       </header>
 
       {pokemonPartyData ? (
         <div className="pokemon-party-list">
-          {/* <ul className="pokemon-party-member"> */}
-            <li className='pokemon-party-member'><a href='#'><img src={pokemonPartyData.data.attributes.small_img}/> {pokemonPartyData.data.attributes.name}, Level: {pokemonPartyData.data.attributes.level}</a></li>
-            {/* <li className='pokemon-party-member'> </li>
-            <li className='pokemon-party-member'> </li>
-            <li className='pokemon-party-member'> </li>
-            <li className='pokemon-party-member'> </li>
-            <li className='pokemon-party-member'> </li> */}
-          {/* </ul> */}
+            <ul className='pokemon-party-member'><a href='#'><img src={pokemonPartyData.data.attributes.small_img}/> {pokemonPartyData.data.attributes.name}, Level: {pokemonPartyData.data.attributes.level}</a></ul>
         </div>
       ) : (
         <h1 className="pokemon-load-error">Loading Pokémon data...</h1>
@@ -52,3 +42,57 @@ function PartyMenu() {
 }
 
 export default PartyMenu;
+
+// BELOW IS AN ALTRNATIVE VERSION TO RENDER ALL POKEMON IN DB, NOT JUST CHARMANDER
+// import './PartyMenu.css';
+// import { useState, useEffect } from "react";
+
+// function PartyMenu() {
+//   const [pokemonPartyData, setPokemonPartyData] = useState(null);
+
+//   function fetchData() {
+//     fetch("http://127.0.0.1:3000/api/v1/trainers/1/pokemons")
+//       .then(response => {
+//         console.log("Received response:", response);
+//         return response.json();
+//       })
+//       .then(data => {
+//         console.log("Parsed data:", data);
+//         setPokemonPartyData(data); 
+//       })
+//       .catch(error => {
+//         console.error('Fetch operation failed:', error);
+//       });
+//   }
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div className="party-menu">
+//       <header className="party-header">
+//         Your Party!
+//       </header>
+
+//       {pokemonPartyData ? (
+//         <div className="pokemon-party-list">
+//           <ul>
+//             {pokemonPartyData.data.map((pokemon) => (
+//               <li key={pokemon.id} className='pokemon-party-member'>
+//                 <a href="#">
+//                   <img src={pokemon.attributes.small_img} alt={pokemon.attributes.name} />
+//                   {pokemon.attributes.name}, Level: {pokemon.attributes.level}
+//                 </a>
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//       ) : (
+//         <h1 className="pokemon-load-error">Loading Pokémon data...</h1>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default PartyMenu;
