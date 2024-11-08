204f05f5cff2ef1e6cebb87ec12f637fe360ca1a
Kyle Delaney <khdelaney99@gmail.com>
2024-11-08 18:09:11 -0500

    add party cypress test

--- cypress/e2e/party.cy.js
+++ cypress/e2e/party.cy.js
@@ -2,8 +2,40 @@ describe('party spec', () => {
   beforeEach(() => {
     cy.intercept('GET', 'https://obscure-caverns-08355-6f81aa04bbe3.herokuapp.com/api/v1/trainers/1/pokemons/2', {
       statusCode: 200,
+      fixture: 'single_pokemon'
+    }).as('getSinglePokemon');
+
+    cy.intercept('GET', 'https://obscure-caverns-08355-6f81aa04bbe3.herokuapp.com/api/v1/trainers/1/pokemons', {
+      statusCode: 200,
       fixture: 'PkmnData'
     }).as('getParty');
+
     cy.visit('http://localhost:3000/');
-})
+    cy.get('.start-words').click()
+  })
+
+  it('should have a party button my play screen', () => {
+    cy.get('.party-button > img').should('exist')
+  })
+  it('should render a party modal when clicked', () => {
+    cy.get('.party-button > img').click()
+    cy.get('.modal-content').should('exist')
+    cy.get('.modal-header').contains('Party')
+    cy.get('.modal-body').should('exist')
+    cy.get('ul > :nth-child(1)').contains('Squirtle, Level: 7')
+    cy.get('ul > :nth-child(2)').contains('Charmander, Level: 27')
+    cy.get('ul > :nth-child(3)').contains('Bulbasaur, Level: 8')
+    cy.get('.modal-footer').contains('Close')
+  })
+  it('Should allow the user to exist the modal on click', ()=> {
+    cy.get('.party-button > img').click()
+    cy.get('.modal-content').should('exist')
+    cy.get('.btn-close').click()
+    cy.get('.modal-content').should('not.exist')
+  })
+  it('Should allow the user to swap pokemon on the screen when clicked', () => {
+    cy.get('.party-button > img').click()
+    cy.get('ul > :nth-child(1)').click()
+    cy.get('.pokemon-name-level').contains('Squirtle, Level: 7')
+  })
 });
