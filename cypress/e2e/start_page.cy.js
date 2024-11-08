4ed880dc6615347d0a3c4e5f60f343a71692342f
Kyle Delaney <khdelaney99@gmail.com>
2024-11-08 15:28:08 -0500

    add test for start page

--- cypress/e2e/start_page.cy.js
+++ cypress/e2e/start_page.cy.js
@@ -5,15 +5,17 @@ describe('Start spec', () => {
       fixture: 'PkmnData'
     }).as('getParty');
     cy.visit('http://localhost:3000/');
-})
+  })
 
+  it('Should correctly show title screen on start page',()=>{
+    cy.get('.Logo').should('be.visible')
+    .get('.start-header').should('be.visible')
+    .get('.start-words').should('have.text', 'Current element is the start page.Click here to advance to the game!')
+  })
 
-it('Should correctly show title screen on start page',()=>{
-  
-})
-it('Should correctly show logo and entry buttons on the start page.', ()=>
-{
-
-})
+  it('Should take the user to the play area when the entry button is clicked', () => {
+    cy.get('.start-words').click()
+    .get('.play-container').should('be.visible')
+  })
 
 });
