79f6aa9f95380ca0668e4b11003bb0066e76ec06
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-11-08 13:57:14 -0700

    Fix: Rewrote fixture files to reflect json response

--- cypress/e2e/start_page.cy.js
+++ cypress/e2e/start_page.cy.js
@@ -5,23 +5,22 @@ describe('Start spec', () => {
       fixture: 'PkmnData'
     }).as('getParty');
     cy.visit('http://localhost:3001/');
-})
+  })
 
-it('Should correctly show title screen on start page',()=>{
-  cy.get('.Logo').should("exist")
-})
+  it('Should correctly show title screen on start page', () => {
+    cy.get('.Logo').should("exist")
+  })
 
-it('Should correctly show logo and entry buttons on the start page.', ()=>
-{
-  cy.get('.Logo').should("exist")
-  cy.get('.start-header').should("exist")
-  cy.get('.start-words').should("exist").and('contain', 'Current element is the start page.')
-})
+  it('Should correctly show logo and entry buttons on the start page.', () => {
+    cy.get('.Logo').should("exist")
+    cy.get('.start-header').should("exist")
+    cy.get('.start-words').should("exist").and('contain', 'Current element is the start page.')
+  })
 
-it('Should navigate to play screen when link is clicked', ()=>{
-  cy.get('.start-words').should("exist").click();
-  cy.get('.play-container').should("be.visible");
-  cy.get('.Logo').should("be.visible");
-})
+  it('Should navigate to play screen when link is clicked', () => {
+    cy.get('.start-words').should("exist").click();
+    cy.get('.play-container').should("be.visible");
+    cy.get('.Logo').should("be.visible");
+  })
 
 });
