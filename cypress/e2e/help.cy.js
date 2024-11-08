5f6b08ac2fd806f1568e13bee15966bfba2ae0e0
ROlearyPro <rolearyprogramming@gmail.com>
2024-11-08 14:03:03 -0700

    Good!

--- cypress/e2e/help.cy.js
+++ cypress/e2e/help.cy.js
@@ -2,7 +2,7 @@ describe('help spec', () => {
   beforeEach(() => {
     cy.intercept('GET', 'https://obscure-caverns-08355-6f81aa04bbe3.herokuapp.com/api/v1/trainers/1/pokemons/2', {
       statusCode: 200,
-      fixture: 'PkmnData'
+      fixture: 'single_pokemon'
     }).as('getParty');
     cy.visit('http://localhost:3000/');
     cy.get('.start-words').click();
